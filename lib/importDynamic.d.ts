980f6a5f6a23ac6e15386e6b03459a97cbf188cc
typhonrt <support@typhonrt.org>
2021-03-04 00:03:16 -0800

    initial commit

---
+++ lib/importDynamic.d.ts
@@ -0,0 +1,8 @@
+declare const _default: Function;
+/**
+ * Provides a mechanism to use dynamic import / import() with tsconfig -> module: commonJS as otherwise import() gets
+ * transpiled to require().
+ *
+ * Simply import and use in the same manner as import().
+ */
+export default _default;
